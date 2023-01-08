f31169ea41872e3e0fd59ca4eca749f17aaab5a9
litthy <litthy@qq.com>
2023-01-08 15:07:57 +0800

    feat: server

---
+++ components/page/appliance.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="m-item-appliance" :class="data.direction">
+        <img :src="require(`../../static/images/${data.bg}`)" class="img" />
+        <div class="box">
+            <span class="title">{{ data.label }}</span>
+            <span class="desc">{{ data.desc }}</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ["data"],
+
+    methods: {},
+};
+</script>
+
+<style lang="less">
+.m-item-appliance {
+    .flex;
+    .size(100%,295px);
+    .r(5px);
+    gap: 35px;
+    padding: 52px 60px;
+    box-sizing: border-box;
+    background-color: #fff;
+    .img {
+        .size(388px,190px);
+    }
+    .box {
+        .flex;
+        flex-direction: column;
+        .title {
+            .fz(21px,27px);
+        }
+        .desc {
+            .mt(14px);
+            .fz(16px,25px);
+            color: #666;
+        }
+    }
+    &.right {
+        .img {
+            order: 1;
+        }
+    }
+}
+</style>
--- pages/server.vue
+++ pages/server.vue
@@ -6,6 +6,24 @@
         <client-only>
             <!-- 轮播 -->
             <index-carousel></index-carousel>
+            <!-- tabs -->
+            <page-tabs :data="tabs" :active="active" @update="update"></page-tabs>
+            <!-- 产品介绍 -->
+            <div v-for="(item, key) in server" :key="key" :id="key">
+                <page-title :data="{ title: item.title }"></page-title>
+
+                <template v-if="item.list">
+                    <div class="m-content wp">
+                        <component
+                            :is="showModal(key)"
+                            v-for="(_item, i) in item.list"
+                            :key="i"
+                            :data="_item"
+                        ></component>
+                    </div>
+                </template>
+                <component v-else :is="showModal(key)" :data="item"></component>
+            </div>
         </client-only>
 
         <!-- 公共底部 -->
@@ -14,14 +32,55 @@
 </template>
 
 <script>
+import server from "@/assets/data/server.json";
 export default {
     name: "IndexServer",
     data() {
-        return {};
+        return {
+            server,
+            active: "",
+            scenes: "",
+        };
     },
-    computed: {},
-    methods: {},
+    computed: {
+        tabs() {
+            return Object.values(this.server).map((item) => item.title);
+        },
+    },
+    methods: {
+        update(name) {
+            this.active = name;
+            let _name = "";
+            for (const key in this.server) {
+                if (this.server[key].title == name) _name = key;
+            }
+            document.getElementById(_name).scrollIntoView();
+        },
+        showModal(key) {
+            const data = {
+                info: "page-info",
+                figure: "page-figure",
+                core: "page-core",
+                scenes: "page-appliance",
+            };
+            return data[key];
+        },
+    },
+    mounted() {
+        this.active = this.tabs[0];
+    },
 };
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+.p-server {
+    .m-content {
+        .flex;
+        flex-wrap: wrap;
+        gap: 20px;
+    }
+    #scenes{
+        .pb(75px);
+    }
+}
+</style>
